9f60483d82aba9a1e8418660ef473e970970a8db
Omkar035-tech <omkarapandkar4u@gmail.com>
2025-03-06 02:19:52 +0530

    Add database configuration for postgres database

---
+++ backend/src/utils/db.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+);
+
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Database connection successful! 🎉');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+testConnection();
+
+export default sequelize;
